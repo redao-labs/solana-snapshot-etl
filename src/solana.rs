abd840344aa00917fdb3da1adb371fe2738fc837
Full Name <email@address.com>
2025-04-20 23:13:18 +0200

    solanars

--- src/solana.rs
+++ src/solana.rs
@@ -13,107 +13,263 @@
 // limitations under the License.
 
 // This file contains code vendored from https://github.com/solana-labs/solana
+// Source: solana/runtime/src/append_vec.rs
 
-use bincode::Options;
-use serde::de::DeserializeOwned;
-use serde::Deserialize;
-use solana_runtime::accounts_db::BankHashInfo;
-use solana_runtime::ancestors::AncestorsForSerialization;
-use solana_runtime::append_vec::StoredMetaWriteVersion;
-use solana_runtime::blockhash_queue::BlockhashQueue;
-use solana_runtime::epoch_stakes::EpochStakes;
-use solana_runtime::rent_collector::RentCollector;
-use solana_runtime::stakes::Stakes;
-use solana_sdk::clock::{Epoch, UnixTimestamp};
-use solana_sdk::deserialize_utils::default_on_eof;
-use solana_sdk::epoch_schedule::EpochSchedule;
-use solana_sdk::fee_calculator::{FeeCalculator, FeeRateGovernor};
-use solana_sdk::hard_forks::HardForks;
-use solana_sdk::hash::Hash;
-use solana_sdk::inflation::Inflation;
-use solana_sdk::pubkey::Pubkey;
-use solana_sdk::slot_history::Slot;
-use solana_sdk::stake::state::Delegation;
-use std::collections::{HashMap, HashSet};
-use std::io::Read;
+use {
+    log::*,
+    memmap2::{Mmap, MmapMut},
+    serde::{Deserialize, Serialize},
+    solana_sdk::{
+        account::{Account, AccountSharedData, ReadableAccount},
+        clock::Epoch,
+        hash::Hash,
+        pubkey::Pubkey,
+    },
+    std::{
+        convert::TryFrom,
+        fs::OpenOptions,
+        io::{self, Read},
+        mem,
+        path::Path,
+    },
+};
 
-const MAX_STREAM_SIZE: u64 = 32 * 1024 * 1024 * 1024;
-
-pub fn deserialize_from<R, T>(reader: R) -> bincode::Result<T>
-where
-    R: Read,
-    T: DeserializeOwned,
-{
-    bincode::options()
-        .with_limit(MAX_STREAM_SIZE)
-        .with_fixint_encoding()
-        .allow_trailing_bytes()
-        .deserialize_from::<R, T>(reader)
+// Data placement should be aligned at the next boundary. Without alignment accessing the memory may
+// crash on some architectures.
+pub const ALIGN_BOUNDARY_OFFSET: usize = mem::size_of::<u64>();
+macro_rules! u64_align {
+    ($addr: expr) => {
+        ($addr + (ALIGN_BOUNDARY_OFFSET - 1)) & !(ALIGN_BOUNDARY_OFFSET - 1)
+    };
 }
 
-#[derive(Default, PartialEq, Eq, Debug, Deserialize)]
-struct UnusedAccounts {
-    unused1: HashSet<Pubkey>,
-    unused2: HashSet<Pubkey>,
-    unused3: HashMap<Pubkey, u64>,
+pub const MAXIMUM_APPEND_VEC_FILE_SIZE: u64 = 16 * 1024 * 1024 * 1024; // 16 GiB
+
+pub type StoredMetaWriteVersion = u64;
+
+// NOTE: taken from https://github.com/solana-labs/solana/blob/8c860e98949efafee878acfac9da67b0bf927cac/runtime/src/account_storage/meta.rs#L212
+/// Meta contains enough context to recover the index from storage itself
+/// This struct will be backed by mmaped and snapshotted data files.
+/// So the data layout must be stable and consistent across the entire cluster!
+#[derive(Clone, PartialEq, Eq, Debug)]
+#[repr(C)]
+pub struct StoredMeta {
+    /// global write version
+    /// This will be made completely obsolete such that we stop storing it.
+    /// We will not support multiple append vecs per slot anymore, so this concept is no longer necessary.
+    /// Order of stores of an account to an append vec will determine 'latest' account data per pubkey.
+    pub write_version: StoredMetaWriteVersion,
+    pub data_len: u64,
+    /// key for the account
+    pub pubkey: Pubkey,
 }
 
-#[derive(Deserialize)]
-#[allow(dead_code)]
-pub struct DeserializableVersionedBank {
-    pub blockhash_queue: BlockhashQueue,
-    pub ancestors: AncestorsForSerialization,
-    pub hash: Hash,
-    pub parent_hash: Hash,
-    pub parent_slot: Slot,
-    pub hard_forks: HardForks,
-    pub transaction_count: u64,
-    pub tick_height: u64,
-    pub signature_count: u64,
-    pub capitalization: u64,
-    pub max_tick_height: u64,
-    pub hashes_per_tick: Option<u64>,
-    pub ticks_per_slot: u64,
-    pub ns_per_slot: u128,
-    pub genesis_creation_time: UnixTimestamp,
-    pub slots_per_year: f64,
-    pub accounts_data_len: u64,
-    pub slot: Slot,
-    pub epoch: Epoch,
-    pub block_height: u64,
-    pub collector_id: Pubkey,
-    pub collector_fees: u64,
-    pub fee_calculator: FeeCalculator,
-    pub fee_rate_governor: FeeRateGovernor,
-    pub collected_rent: u64,
-    pub rent_collector: RentCollector,
-    pub epoch_schedule: EpochSchedule,
-    pub inflation: Inflation,
-    pub stakes: Stakes<Delegation>,
-    #[allow(dead_code)]
-    unused_accounts: UnusedAccounts,
-    pub epoch_stakes: HashMap<Epoch, EpochStakes>,
-    pub is_delta: bool,
+/// This struct will be backed by mmaped and snapshotted data files.
+/// So the data layout must be stable and consistent across the entire cluster!
+#[derive(Serialize, Deserialize, Clone, Debug, Default, Eq, PartialEq)]
+#[repr(C)]
+pub struct AccountMeta {
+    /// lamports in the account
+    pub lamports: u64,
+    /// the epoch at which this account will next owe rent
+    pub rent_epoch: Epoch,
+    /// the program that owns this account. If executable, the program that loads this account.
+    pub owner: Pubkey,
+    /// this account's data contains a loaded program (and is now read-only)
+    pub executable: bool,
 }
 
-#[derive(Clone, Debug, Default, Deserialize, PartialEq)]
-pub struct AccountsDbFields<T>(
-    pub HashMap<Slot, Vec<T>>,
-    pub StoredMetaWriteVersion,
-    pub Slot,
-    pub BankHashInfo,
-    /// all slots that were roots within the last epoch
-    #[serde(deserialize_with = "default_on_eof")]
-    pub Vec<Slot>,
-    /// slots that were roots within the last epoch for which we care about the hash value
-    #[serde(deserialize_with = "default_on_eof")]
-    pub Vec<(Slot, Hash)>,
-);
+impl<'a, T: ReadableAccount> From<&'a T> for AccountMeta {
+    fn from(account: &'a T) -> Self {
+        Self {
+            lamports: account.lamports(),
+            owner: *account.owner(),
+            executable: account.executable(),
+            rent_epoch: account.rent_epoch(),
+        }
+    }
+}
 
-pub type SerializedAppendVecId = usize;
+impl<'a, T: ReadableAccount> From<Option<&'a T>> for AccountMeta {
+    fn from(account: Option<&'a T>) -> Self {
+        match account {
+            Some(account) => AccountMeta::from(account),
+            None => AccountMeta::default(),
+        }
+    }
+}
 
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, Deserialize)]
-pub struct SerializableAccountStorageEntry {
-    pub id: SerializedAppendVecId,
-    pub accounts_current_len: usize,
+/// References to account data stored elsewhere. Getting an `Account` requires cloning
+/// (see `StoredAccountMeta::clone_account()`).
+#[derive(PartialEq, Eq, Debug)]
+pub struct StoredAccountMeta<'a> {
+    pub meta: &'a StoredMeta,
+    /// account data
+    pub account_meta: &'a AccountMeta,
+    pub data: &'a [u8],
+    pub offset: usize,
+    pub stored_size: usize,
+    pub hash: &'a Hash,
+}
+
+impl<'a> StoredAccountMeta<'a> {
+    /// Return a new Account by copying all the data referenced by the `StoredAccountMeta`.
+    pub fn clone_account(&self) -> AccountSharedData {
+        AccountSharedData::from(Account {
+            lamports: self.account_meta.lamports,
+            owner: self.account_meta.owner,
+            executable: self.account_meta.executable,
+            rent_epoch: self.account_meta.rent_epoch,
+            data: self.data.to_vec(),
+        })
+    }
+}
+
+/// A thread-safe, file-backed block of memory used to store `Account` instances. Append operations
+/// are serialized such that only one thread updates the internal `append_lock` at a time. No
+/// restrictions are placed on reading. That is, one may read items from one thread while another
+/// is appending new items.
+pub struct AppendVec {
+    /// A file-backed block of memory that is used to store the data for each appended item.
+    map: Mmap,
+
+    /// The number of bytes used to store items, not the number of items.
+    current_len: usize,
+
+    /// The number of bytes available for storing items.
+    file_size: u64,
+}
+
+impl AppendVec {
+    fn sanitize_len_and_size(current_len: usize, file_size: usize) -> io::Result<()> {
+        if file_size == 0 {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("too small file size {} for AppendVec", file_size),
+            ))
+        } else if usize::try_from(MAXIMUM_APPEND_VEC_FILE_SIZE)
+            .map(|max| file_size > max)
+            .unwrap_or(true)
+        {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("too large file size {} for AppendVec", file_size),
+            ))
+        } else if current_len > file_size {
+            Err(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                format!("current_len is larger than file size ({})", file_size),
+            ))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// how many more bytes can be stored in this append vec
+    pub fn remaining_bytes(&self) -> u64 {
+        (self.capacity()).saturating_sub(self.len() as u64)
+    }
+
+    pub fn len(&self) -> usize {
+        self.current_len
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len() == 0
+    }
+
+    pub fn capacity(&self) -> u64 {
+        self.file_size
+    }
+
+    pub fn new_from_file<P: AsRef<Path>>(path: P, current_len: usize) -> io::Result<Self> {
+        let data = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .create(false)
+            .open(&path)?;
+
+        let file_size = std::fs::metadata(&path)?.len();
+        AppendVec::sanitize_len_and_size(current_len, file_size as usize)?;
+
+        let map = unsafe {
+            let result = Mmap::map(&data);
+            if result.is_err() {
+                // for vm.max_map_count, error is: {code: 12, kind: Other, message: "Cannot allocate memory"}
+                info!("memory map error: {:?}. This may be because vm.max_map_count is not set correctly.", result);
+            }
+            result?
+        };
+
+        let new = AppendVec {
+            map,
+            current_len,
+            file_size,
+        };
+
+        Ok(new)
+    }
+
+    pub fn new_from_reader<R: Read>(reader: &mut R, current_len: usize) -> io::Result<Self> {
+        let mut map = MmapMut::map_anon(current_len)?;
+        io::copy(&mut reader.take(current_len as u64), &mut map.as_mut())?;
+        Ok(AppendVec {
+            map: map.make_read_only()?,
+            current_len,
+            file_size: current_len as u64,
+        })
+    }
+
+    /// Get a reference to the data at `offset` of `size` bytes if that slice
+    /// doesn't overrun the internal buffer. Otherwise return None.
+    /// Also return the offset of the first byte after the requested data that
+    /// falls on a 64-byte boundary.
+    fn get_slice(&self, offset: usize, size: usize) -> Option<(&[u8], usize)> {
+        let (next, overflow) = offset.overflowing_add(size);
+        if overflow || next > self.len() {
+            return None;
+        }
+        let data = &self.map[offset..next];
+        let next = u64_align!(next);
+
+        Some((
+            //UNSAFE: This unsafe creates a slice that represents a chunk of self.map memory
+            //The lifetime of this slice is tied to &self, since it points to self.map memory
+            unsafe { std::slice::from_raw_parts(data.as_ptr() as *const u8, size) },
+            next,
+        ))
+    }
+
+    /// Return a reference to the type at `offset` if its data doesn't overrun the internal buffer.
+    /// Otherwise return None. Also return the offset of the first byte after the requested data
+    /// that falls on a 64-byte boundary.
+    fn get_type<'a, T>(&self, offset: usize) -> Option<(&'a T, usize)> {
+        let (data, next) = self.get_slice(offset, mem::size_of::<T>())?;
+        let ptr: *const T = data.as_ptr() as *const T;
+        //UNSAFE: The cast is safe because the slice is aligned and fits into the memory
+        //and the lifetime of the &T is tied to self, which holds the underlying memory map
+        Some((unsafe { &*ptr }, next))
+    }
+
+    /// Return account metadata for the account at `offset` if its data doesn't overrun
+    /// the internal buffer. Otherwise return None. Also return the offset of the first byte
+    /// after the requested data that falls on a 64-byte boundary.
+    pub fn get_account<'a>(&'a self, offset: usize) -> Option<(StoredAccountMeta<'a>, usize)> {
+        let (meta, next): (&'a StoredMeta, _) = self.get_type(offset)?;
+        let (account_meta, next): (&'a AccountMeta, _) = self.get_type(next)?;
+        let (hash, next): (&'a Hash, _) = self.get_type(next)?;
+        let (data, next) = self.get_slice(next, meta.data_len as usize)?;
+        let stored_size = next - offset;
+        Some((
+            StoredAccountMeta {
+                meta,
+                account_meta,
+                data,
+                offset,
+                stored_size,
+                hash,
+            },
+            next,
+        ))
+    }
 }
